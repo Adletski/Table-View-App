6025090d5ba1d6151285a8b346cafa866fa70e5a
Adlet <adlet.zh19966@gmail.com>
2023-03-26 20:51:57 +0600

    Initial Commit

---
+++ Table View app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table View app
+//
+//  Created by Adlet Zhantassov on 26.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
